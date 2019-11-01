b55d83091e702c42581689b1d9e459c38c71d950
romanjrdykyj <romanjrdykyj@gmail.com>
2019-11-01 16:24:27 +0100

    Layout_menu
    Zrobiony layout menu, nie działa fullscreen. NAPRAWIĆ!!

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,17 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlText;
+            this.button1.BackgroundImage = global::DontScrewAround.Properties.Resources.ws_511_zero_1024_4;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F);
+            this.button1.Location = new System.Drawing.Point(465, 200);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(350, 120);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Nowa gra";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ControlText;
+            this.button2.BackgroundImage = global::DontScrewAround.Properties.Resources.ws_511_zero_1024_4;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F);
+            this.button2.Location = new System.Drawing.Point(465, 377);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(350, 120);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Wyniki";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ControlText;
+            this.button3.BackgroundImage = global::DontScrewAround.Properties.Resources.ws_511_zero_1024_4;
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F);
+            this.button3.Location = new System.Drawing.Point(465, 552);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(350, 120);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Opcje";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.ControlText;
+            this.button4.BackgroundImage = global::DontScrewAround.Properties.Resources.ws_511_zero_1024_4;
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F);
+            this.button4.Location = new System.Drawing.Point(465, 722);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(350, 120);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Wyjście";
+            this.button4.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(640, 532);
+            this.BackgroundImage = global::DontScrewAround.Properties.Resources.tło_menuDPI1280;
+            this.ClientSize = new System.Drawing.Size(1280, 1024);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.MaximumSize = new System.Drawing.Size(1280, 1024);
-            this.MinimumSize = new System.Drawing.Size(639, 511);
+            this.MinimumSize = new System.Drawing.Size(1278, 982);
             this.Name = "Form1";
             this.ShowInTaskbar = false;
             this.Text = "DontScrewAround";
@@ -50,6 +115,11 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
