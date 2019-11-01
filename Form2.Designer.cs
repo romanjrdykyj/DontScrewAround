cc7c71cc49e599d170f0a5a18c50dc0953d5f55e
romanjrdykyj <romanjrdykyj@gmail.com>
2019-11-01 23:32:44 +0100

    Timer do zapamietywania

--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -28,7 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(1098, 18);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(2);
+            this.label1.Size = new System.Drawing.Size(138, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Zapamiętaj: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label2.Location = new System.Drawing.Point(565, 966);
+            this.label2.Name = "label2";
+            this.label2.Padding = new System.Windows.Forms.Padding(2);
+            this.label2.Size = new System.Drawing.Size(91, 32);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Oblicz: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label3.Location = new System.Drawing.Point(35, 18);
+            this.label3.Name = "label3";
+            this.label3.Padding = new System.Windows.Forms.Padding(2);
+            this.label3.Size = new System.Drawing.Size(90, 32);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Wynik: ";
             // 
             // Form2
             // 
@@ -36,6 +86,9 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.BackgroundImage = global::DontScrewAround.Properties.Resources.layout_rozgrywki;
             this.ClientSize = new System.Drawing.Size(1280, 1024);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.MaximumSize = new System.Drawing.Size(1280, 1024);
@@ -46,9 +99,15 @@
             this.TopMost = true;
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
--- Form2.cs
+++ Form2.cs
@@ -12,9 +12,54 @@ namespace DontScrewAround
 {
     public partial class Form2 : Form
     {
+
+        public int ratunek_licznik_odstępu = 100;
+        public int ratunek_licznik_widocznosci = 0;
+
+
         public Form2()
         {
             InitializeComponent();
+            timer1.Start();
+
+        }
+
+        private void Form2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape) // wychodzenie z trybu pelnoekranowego
+            {
+                new Form1().ShowDialog();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+
+                if (ratunek_licznik_odstępu == 100)
+                {
+                    Random rnd = new Random();
+                    int poczatek = 1;
+                    int koniec = 49;
+                    int wylosowana = rnd.Next(poczatek, koniec);
+                    label1.Text = "Zapamiętaj: " + wylosowana.ToString();
+                    ratunek_licznik_odstępu = 0;
+                }
+                if(ratunek_licznik_widocznosci < 20)
+                {
+                label1.Visible = true;
+                }
+                if(ratunek_licznik_widocznosci < 100 & ratunek_licznik_widocznosci > 20)
+            {
+                label1.Visible = false;
+            }
+                if(ratunek_licznik_widocznosci == 100)
+            {
+                ratunek_licznik_widocznosci = 0;
+            }
+                ratunek_licznik_odstępu += 1;
+            ratunek_licznik_widocznosci += 1;
+            
         }
     }
 }
